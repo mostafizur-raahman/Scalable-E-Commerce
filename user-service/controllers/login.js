cb0db3a51c0e1b692484987bc5c0f0a9be8a3456
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-10-25 10:50:32 +0600

    feat: add user login

--- user-service/controllers/login.js
+++ user-service/controllers/login.js
@@ -1,5 +1,8 @@
 const { ApiResponse } = require("../middleware/ApiResponse");
 const userModel = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../config");
 
 exports.userLogin = async (req, res, next) => {
     try {
@@ -11,17 +14,28 @@ exports.userLogin = async (req, res, next) => {
         });
 
         if (!user) {
-            return ApiResponse(res, 404, false, "User not found");
+            ApiResponse(res, 404, false, "User not found");
         }
 
         if (!email || !password) {
-            return ApiResponse(
-                res,
-                404,
-                false,
-                "Email and password are required"
-            );
+            ApiResponse(res, 404, false, "Email and password are required");
         }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            ApiResponse(res, 400, false, "Invalid credentials");
+        }
+
+        const token = jwt.sign(
+            { _id: user._id, email: user.email },
+            config.jwt_secret,
+            { expiresIn: config.jwt_ttl }
+        );
+
+        return res.status(200).json({
+            token,
+        });
     } catch (error) {
         next(error);
     }
--- user-service/routes/index.js
+++ user-service/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { registerUser } = require("../controllers/register");
+const { userLogin } = require("../controllers/login");
 
 const userRoutes = new Router();
 
 userRoutes.post("/register", registerUser);
+userRoutes.post("/login", userLogin);
 
 module.exports = userRoutes;
