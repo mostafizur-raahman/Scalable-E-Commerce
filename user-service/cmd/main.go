83f1763e87f95b33945d5f7dcc4e5c19dceea141
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2025-01-30 23:09:18 +0600

    added main.go

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+
+	"user-service/config"
+	"user-service/database"
+	"user-service/internal/routes"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Try loading `.env` manually
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error on loading env")
+		log.Println("⚠️ Warning: No .env file found, using system environment variables")
 	}
+
+	cfg := config.LoadConfig()
+
+	database.ConnectDatabase(cfg)
+
+	routes.UserRoutes()
+
+	serverAddr := ":" + cfg.ServerPort
+	fmt.Printf("✅ User service is running on port %s\n", cfg.ServerPort)
+	log.Fatal(http.ListenAndServe(serverAddr, nil))
 }
